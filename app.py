1331013765615d0146e22e3fc489416d3365246c
satinder <satindersingh472@gmail.com>
2023-01-27 22:41:00 -0700

    added_files_from_codecademy

---
+++ app.py
@@ -0,0 +1,32 @@
+from nile import get_distance, format_price, SHIPPING_PRICES
+from test import test_function
+
+# Define calculate_shipping_cost() here:
+def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
+  from_lat, from_long = from_coords
+  to_lat, to_long = to_coords
+  distance = get_distance(from_lat, from_long, to_long, to_lat)
+  shipping_rate = SHIPPING_PRICES[shipping_type]
+  price = distance * shipping_rate
+  return format_price(price)
+
+  
+
+# Test the function by calling 
+test_function(calculate_shipping_cost)
+
+
+
+# Define calculate_driver_cost() here
+def calculate_driver_cost(distance, *drivers):
+  cheapest_driver = None
+  cheapest_driver_price
+
+# Test the function by calling 
+# test_function(calculate_driver_cost)
+
+# Define calculate_money_made() here
+
+
+# Test the function by calling 
+# test_function(calculate_money_made)
